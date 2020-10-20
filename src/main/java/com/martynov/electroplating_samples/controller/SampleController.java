ef5ed2b35769a95592f997106ad24115bab586be
Martynov_PC <martynov.vd@yandex.ru>
2020-10-20 19:19:08 +0300

    minor changes

---
+++ src/main/java/com/martynov/electroplating_samples/controller/SampleController.java
@@ -0,0 +1,49 @@
+package com.martynov.electroplating_samples.controller;
+
+import com.martynov.electroplating_samples.entity.Sample;
+import com.martynov.electroplating_samples.service.SampleServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class SampleController {
+    private final SampleServiceImpl sampleService;
+
+    @Autowired
+    public SampleController (SampleServiceImpl sampleService) {
+        this.sampleService = sampleService;
+    }
+
+    @PostMapping
+    public void addSample(@RequestBody Sample sample) {
+        sampleService.add(sample);
+    }
+
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Sample> getAll() {
+        return sampleService.getAll();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Sample getById (@PathVariable int id) {
+        return sampleService.getById(id);
+    }
+
+    @GetMapping(value = "/amp/{amp}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Sample> getByAmp (@PathVariable String amp) {
+        return sampleService.getByAmp(amp);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateById(@PathVariable int id, @RequestBody Sample sample) {
+        sampleService.updateById(id, sample);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteById(@PathVariable int id) {
+        sampleService.deleteById(id);
+    }
+}
